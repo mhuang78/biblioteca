ffef2ec77a5bab59fd42cd083872022341ce3d0c
mhuang78 <87772003+mhuang78@users.noreply.github.com>
2023-11-08 09:02:31 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,154 +1,185 @@
 import csv
+from datetime import datetime
   
 
 # função que carrega arquivo csv
-def carregar_csv(filename): 
-    with open(filename, mode='r') as csv_file:
+def carregar_csv(arquivo): 
+    with open(arquivo, mode='r') as csv_file:
         reader = csv.DictReader(csv_file)
-        for row in reader:
-            biblioteca.append(row)
+        for linha in reader:
+            biblioteca.append(linha)
+
+# função para mostrar a biblioteca
+
+def extrato_biblioteca(arquivo):
+    print(f"\n{'Titulo':<40} {'Autor':<25}{'Categoria':<15}{'Valor':<10}{'Data'}")
+    print('---------------------------------------------------------------------------------------------------------')
+    for livro in arquivo:        
+        print(f"{livro['Titulo']:<40} {livro['Autor']:<25} {livro['Categoria']:<15}{livro['Valor']:<10}{livro['Data']}")
+    print('\n')
 
 
 # Verificar se ja existe biblioteca existente. Se não, criar arquivo csv novo
-biblioteca_existente = 0
-while biblioteca_existente == 0:
-    biblioteca_existente = int(input('Já existe uma biblioteca criada anteriormente? 1- Sim, 2- Não: '))
-    if biblioteca_existente == 1:
+biblioteca_existente = '0'
+while biblioteca_existente == '0':
+    print(f'\nAtenção!. Criar uma nova biblioteca vai apagar biblioteca existente. ')
+    biblioteca_existente = input('\nJá existe uma biblioteca? 123- Sim, 456- Não: ')
+    if biblioteca_existente == '123':
         # Comando para importar a biblioteca existente
         biblioteca = []
-        csv_file_path = 'C:/Users/marco/projeto_restaurante/biblioteca.csv'
-        carregar_csv(csv_file_path)
-    elif biblioteca_existente == 2:
+        csv_path = 'biblioteca.csv'
+        carregar_csv(csv_path)
+    elif biblioteca_existente == '456':        
         biblioteca = []
     else:
-        print('Digite OPÇÃO VÁLIDA.')
+        print('\nDigite OPÇÃO VÁLIDA.')
+        biblioteca_existente = '0'
 
 
 # função para adicionar livro
-def adicionar_livro(titulo, autor, categoria, valor):
-    livro = { 'Titulo': titulo,'Autor': autor, 'Categoria': categoria, 'Valor': valor}
+def adicionar_livro(titulo, autor, categoria, valor, data):
+    
+    livro = { 'Titulo': titulo,'Autor': autor, 'Categoria': categoria, 'Valor': valor, 'Data':data}
     biblioteca.append(livro)
+    salvar_csv('biblioteca.csv')
+
+# função para remover livro pelo titulo
+def remover_livro(titulo):
+    for livro in biblioteca[:]: #itera sobre uma copia para não dar erro, pois se fosse pelo original, cada remoção muda o index, se tiver varios titulos iguais
+        if livro['Titulo'] == titulo:
+            biblioteca.remove(livro)
+            print(f'{titulo} foi removido da biblioteca.')
+            
 
 # função para salvar arquivo csv
-def salvar_csv(filename):
-    with open(filename, mode='w', newline='') as csv_file:
-        dados = ['Titulo', 'Autor', 'Categoria', 'Valor']
-        writer = csv.DictWriter(csv_file, fieldnames=dados)
+def salvar_csv(arquivo):
+    with open(arquivo, mode='w', newline='') as csv_file:
+        header = ['Titulo', 'Autor', 'Categoria', 'Valor','Data']
+        writer = csv.DictWriter(csv_file, fieldnames=header)
 
         writer.writeheader()
         for livro in biblioteca:
             writer.writerow(livro)
 
-
-
                  
 # funçao para pesquisar livro segundo um criterio
-def pesquisar_livros(criteria, value):
-    livro_achado = []
-    
+def pesquisar_livros(header, criterio):
+    livro_achado = []    
     for livro in biblioteca:
-        if livro[criteria] == value:
-            livro_achado.append(livro)
-    
+        if livro[header] == criterio:
+            livro_achado.append(livro)    
     return livro_achado
 
 # função para atualizar um livro
-def update_book_by_title(title, new_autor=None, new_categoria=None, new_valor=None):
+def atualizar_livro(titulo, novo_autor=None, nova_categoria=None, novo_valor=None):
     for livro in biblioteca:
-        if livro['Titulo'] == title:
-            if new_autor is not None:
-                livro['Autor'] = new_autor
-            if new_categoria is not None:
-                livro['Categoria'] = new_categoria
-            if new_valor is not None:
-                livro['Valor'] = new_valor
+        if livro['Titulo'] == titulo:
+            if novo_autor is not None:
+                livro['Autor'] = novo_autor
+            if nova_categoria is not None:
+                livro['Categoria'] = nova_categoria
+            if novo_valor is not None:
+                livro['Valor'] = novo_valor
             return True
     return False
 
 # funçao para organizar e imprimir a biblioteca 
-def sort_books_by_category(category):
-    sorted_books = sorted(biblioteca, key=lambda x: x[category])
-    return sorted_books
+def organizar(ordenar):
+    livros_organizados = sorted(biblioteca, key=lambda x: x[ordenar])
+    return livros_organizados
 
-# Sample usage of the sort function
-# Sort books by author
-#sorted_by_author = sort_books_by_category('Author')
-#print("Books Sorted by Author:")
-#for book in sorted_by_author:
-#    print(f"{book['Title']} by {book['Author']} ({book['Year Written']})")
+# Programa rodando 
+selecionar = '5'
+while selecionar !='8':
+    extrato_biblioteca(biblioteca)
+    selecionar=(input('1-CADASTRAR, 2-REMOVER, 3- ATUALIZAR, 4-BUSCAR, 5-EXTRATO, 6-TOTAL INVESTIDO, 7-ORDENAR, 8-FECHAR \n'))
+    if selecionar=='8':
+        print('Encerrando programa')
 
-# Sort books by title
-#sorted_by_title = sort_books_by_category('Title')
-#print("\nBooks Sorted by Title:")
-#for book in sorted_by_title:
-#    print(f"{book['Title']} by {book['Author']} ({book['Year Written']})")
+    elif selecionar =='1': # Cadastrar
+        print('Digite os dados na seguinte ordem: titulo, autor, categoria, valor.')
+        w= input("Titulo do livro: ").upper()
+        x= input("Autor: ").upper()
+        y= input ("Categoria: ").upper()
+        z= input("Valor pago: ") 
+        now = datetime.now()
+        data = now.strftime('%d-%m-%Y')
+        print(data)
+        if w =='':
+            print('\nFalta o título do livro. Voltando ao menu principal.')
+        else:    
+            adicionar_livro(w,x,y,z,data)        
 
-# Sort books by year
-#sorted_by_year = sort_books_by_category('Year Written')
-#print("\nBooks Sorted by Year:")
-#for book in sorted_by_year:
-#    print(f"{book['Title']} by {book['Author']} ({book['Year Written']})")
+    elif selecionar == '2': # Remover
+        remover = input('Digite o titulo do livro que quer remover: ')
+        remover_livro(remover)
+
+    elif selecionar == '3': # Atualizar
+        w = input('Digite o titulo do livro que deseja atualizar. Se não precisar de correção nas outras áreas, deixe em branco.: ' ).upper()
+        x = (input('Autor correto: ').upper() or None)
+        y = (input('Categoria correta: ').upper() or None)
+        z = (input('Valor correto: ') or None)
+        atualizar_livro(w,x,y,z)
+        if atualizar_livro(w,x,y,z):
+            print(f'{w} atualizado.')
+        else:
+            print(f'{w} livro não encontrado.')
+
+    elif selecionar == '4': # Buscar
+        busca = input('Buscar por 1-titulo, 2-autor, 3-categoria: ')
+        if busca == '1' or busca == '2' or busca == '3':
+            if busca == '1':
+                titulo = input('Digite o titulo: ').upper()
+                if titulo != '':
+                    busca_titulo = pesquisar_livros('Titulo', titulo)
+                    print(f"\nTitulo de livros: {titulo}")
+                    for livro in busca_titulo:
+                        print(f"Autor: {livro['Autor']:<20} Categoria: {livro['Categoria']}")
+                else:
+                    print('Digite novamente')
+            elif busca == '2':
+                autor = input('Digite o autor: ').upper()
+                if autor != '':
+                    busca_autor = pesquisar_livros('Autor', autor)
+                    print(f"\nLivros escritos por: {autor}")
+                    for livro in busca_autor:
+                        print(f"Livro: {livro['Titulo']:<20} Categoria: {livro['Categoria']}")
+                else:
+                    print('Digite novamente')
+            elif busca == '3':
+                categoria = input('Digite o categoria: ').upper()
+                if categoria != '':
+                    busca_categoria = pesquisar_livros('Categoria', categoria)
+                    print(f"\nLivros na categoria: {categoria}")
+                    for livro in busca_categoria:
+                        print(f"Autor: {livro['Autor']:<20} Titulo: {livro['Titulo']}")
+                else:
+                    print('Digite novamente')
+        else:
+            print('Livro não encontrada ou inválida.')
+
+    elif selecionar == '5': # Mostrar 
+        extrato_biblioteca(biblioteca)
+
+    elif selecionar == '6': # Valor da biblioteca
+
+        for sub in biblioteca:
+            sub['Valor'] = float(sub['Valor'])
+        
+        total = sum(sub['Valor'] for sub in biblioteca)
+       
+        print(f'\nTotal investido na biblioteca: R$ {total}.\n')
+    elif selecionar == '7':
+        ordem = input('Escreva como você quer ordenar a biblioteca. Escreva titulo, autor, ou categoria. ').capitalize()
+        if ordem == 'Titulo' or ordem == 'Autor' or ordem == 'Categoria': 
+            extrato_organizado = organizar(ordem)
+            biblioteca = extrato_organizado
+        else:
+            print('Opção inválida. Voltando ao meu principal.')
+
+    else:
+        print('Selecione opção válida.')
 
 
-# Sample usage of the functions
-#adicionar_livro( 'Harry Potter and the Sorcerer\'s Stone','J.K. Rowling', 'fantasia',50)
-#adicionar_livro( '1984', 'George Orwell','ficção', 30)
-#adicionar_livro( 'The Lord of the Rings', 'J.R.R. Tolkien', 'fantasia', 100)
-#w, x, y, z = input("Digite os dados do novo livro, separados por virgula: ").split(',')
-#adicionar_livro(w,x,y,z)
-
-# Sample usage of the search function
-
-# To search by author
-busca_autor = pesquisar_livros('Autor', 'J.K. Rowling')
-print("Livros por J.K. Rowling:")
-for livro in busca_autor: 
-    print(f"{livro['Titulo']} ({livro['Categoria']})")
-
-# To search by title
-busca_titulo = pesquisar_livros('Titulo', '1984')
-print("\nTitulo de livros '1984':")
-for livro in busca_titulo:
-    print(f"{livro['Autor']} ({livro['Categoria']})")
-
-# To search by year
-busca_categoria = pesquisar_livros('Categoria', 'fantasia')
-print("\nLivros na categoria fantasia:")
-for livro in busca_categoria:
-    print(f"{livro['Titulo']} by {livro['Autor']}")
-
-# Save the livro collection to a CSV file
-salvar_csv('biblioteca.csv')
-
-# Load the livro collection from the CSV file (optional)
-# load_from_csv('biblioteca.csv')
-
-# Display the livro collection
-for livro in biblioteca:
-    print(f"{livro['Titulo']} por {livro['Autor']} {livro['Categoria']} comprado por {livro['Valor']}")
-
-
-# Update a livro by title
-book_to_update = "1984"
-new_autor = None
-new_categoria = 'humor'
-
-if update_book_by_title(book_to_update, new_autor, new_categoria):
-    print(f"\nUpdated '{book_to_update}' in the collection.")
-else:
-    print(f"\nBook '{book_to_update}' not found in the collection.")
-
-
-rodar = 1
-while rodar !=0:
-    selecionar=int(input('1-CADASTRAR, 2-REMOVER, 3- ATUALIZAR, 4-BUSCAR, 5-EXTRATO, 6-TOTAL INVESTIDO, 7-ENCERRAR '))
-    if selecionar==7:
-        rodar = 0
-    elif selecionar ==1:
-        print('Digite os dados na seguinte ordem:titulo,autor,categoria,valor. Use o vírgula para separar cada sessão.')
-        print('\nExemplo:Hamlet,William Shakespeare,ficção,20')
-        w, x, y, z = input("Digite os dados do novo livro, separados por virgula: ").split(',')
-        adicionar_livro(w,x,y,z)
 salvar_csv('biblioteca.csv')        
-for livro in biblioteca:
-    print(f"{livro['Titulo']} por {livro['Autor']} ({livro['Categoria']}) comprado por ({livro['Valor']})")
+extrato_biblioteca(biblioteca)
